221c45511d3be5d70788bcac1486238817f38315
Thedand <theedandroid@gmail.com>
2020-08-09 00:20:02 +0300

    1) Updated the design of the main page. 2) Open access to flask_admin(to table User, Task, Role), update role user:admin (active and superuser). 3) Created account page, a link to the task manager has been created in the profile(only active user).

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,8 @@
 from datetime import datetime
 
-from flask import render_template, redirect, url_for, flash
-from flask_login import current_user, login_user, logout_user
+from flask import render_template, redirect, url_for, flash, request
+from flask_login import current_user, login_user, logout_user, login_required
+from werkzeug.urls import url_parse
 
 from app import app, db
 from app.forms import LoginForm, RegistrationForm
@@ -16,13 +17,15 @@ def before_request():
 
 
 @app.route('/', methods=['GET', 'POST'])
-@app.route('/index', methods=['GET', 'POST'])
+@app.route('/home', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    return render_template('home.html', title='Home')
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
@@ -30,7 +33,10 @@ def login():
             flash('Invalid username or password.')
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
-        return redirect(url_for('index'))
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
     return render_template('login.html', title='Login', form=form)
 
 
@@ -55,3 +61,8 @@ def logout():
     flash('You have been logged out.')
     return redirect(url_for('index'))
 
+
+@app.route("/account")
+@login_required
+def account():
+    return render_template('account.html', title='Account')
